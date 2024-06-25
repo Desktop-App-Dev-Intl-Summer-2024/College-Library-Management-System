b5b47bda771ae55dcc14ce1814923834dd86e4fa
Shishir7878 <shishirbk707@gmail.com>
2024-06-25 10:14:04 -0400

    wpf

---
+++ WPF/College-Library-Management-System/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace College_Library_Management_System.Models
+{
+    internal class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string ISBN { get; set; }
+        public string Publisher { get; set; }
+        public int YearPublished { get; set; }
+        public int CopiesAvailable { get; set; }
+    }
+}
+
---
+++ WPF/College-Library-Management-System/ViewModels/LibraryViewModel.cs
@@ -0,0 +1,33 @@
+﻿using College_Library_Management_System.Models;
+using CollegeLibraryManagementSystem.Data;
+using System.Collections.ObjectModel;
+
+namespace College_Library_Management_System.ViewModels
+{
+    internal class LibraryViewModel
+    {
+        private LibraryDataAccess _dataAccess;
+
+        public ObservableCollection<Book> Books { get; set; }
+        public ObservableCollection<Member> Members { get; set; }
+
+        public LibraryViewModel()
+        {
+            _dataAccess = new LibraryDataAccess();
+            Books = new ObservableCollection<Book>(_dataAccess.GetAllBooks());
+            Members = new ObservableCollection<Member>(_dataAccess.GetAllMembers());
+        }
+
+        public void AddBook(Book book)
+        {
+            _dataAccess.AddBook(book);
+            Books.Add(book);
+        }
+
+        public void AddMember(Member member)
+        {
+            _dataAccess.AddMember(member);
+            Members.Add(member);
+        }
+    }
+}
